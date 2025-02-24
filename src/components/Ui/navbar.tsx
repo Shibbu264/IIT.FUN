b10afbae22ecd9a41f938f5f3dd29ad4bdbf44f8
rishabhr4 <118053976+rishabhr4@users.noreply.github.com>
2025-02-25 04:52:38 +0530

    created navbar component

---
+++ src/components/Ui/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav className="bg-secondaryBlack p-4 flex items-center">
+            <div className="flex-1 flex justify-center">
+                <div className="bg-primaryBlack p-2 rounded-md flex space-x-4">
+                    <a href="#about" className="text-white px-3 py-2 rounded-md hover:bg-secondaryBlack focus:bg-secondaryBlack">About</a>
+                    <a href="#community" className="text-white px-3 py-2 rounded-md hover:bg-secondaryBlack focus:bg-secondaryBlack">Community</a>
+                    <a href="#leaderboard" className="text-white px-3 py-2 rounded-md hover:bg-secondaryBlack focus:bg-secondaryBlack">Leaderboard</a>
+                    <a href="#contribute" className="text-white px-3 py-2 rounded-md hover:bg-secondaryBlack focus:bg-secondaryBlack">Start Contributing</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
