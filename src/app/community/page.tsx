23c6b8e224bffb5b45effd957af021044eeed9df
333nischay <nischaysinha261@gmail.com>
2025-02-26 03:25:48 +0530

    banner set

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -9,10 +9,11 @@ import Achievement from '@/components/Community/achievement'
 export default function page() {
     
     return (
-        <div className='flex flex-col w-full mt-16 mx-auto items-center justify-center gap-8'>
+        <div className='flex flex-col w-full mx-auto items-center justify-center gap-8 max-md:gap-3'>
+        
         <Banner/>
         <Getgallery/>
-        <PromoSection className='w-[68.5%] max-md:w-[75%] !m-11 md:!m-11 lg:!m-11' headingClassName='w-[30%]' heading="Join the Community" text="Inter IIT got you Medals?? We got you $$, If Meme-wars was an inter IIT event Come on, we heard you." image="/phone.png" link="https://www.iit.fun"  />
+        <PromoSection className='md:w-[68.5%] max-md:w-4/5 !m-11 md:!m-11 lg:!m-11' headingClassName='w-[30%]' heading="Join the Community" text="Inter IIT got you Medals?? We got you $$, If Meme-wars was an inter IIT event Come on, we heard you." image="/phone.png" link="https://www.iit.fun"  />
         <Achievement/>
         <PartnersDiv/>
         </div>
--- src/components/Community/banner.tsx
+++ src/components/Community/banner.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 const Banner = () => {
   return (
     <>
-    <div className={cn(styles.bannerContainer, 'max-md:!hidden')}>
+    <div className={cn(styles.bannerContainer, 'max-md:!hidden !mt-[10%]')}>
     <div className={styles.firstBanner}>
       Joining IIT.fun: 69% smarter * Joining IIT.fun: 69% smarter * Joining IIT.fun: 69% smarter * Joining IIT.fun: 69% smarter
     </div>
@@ -14,13 +14,13 @@ const Banner = () => {
         WE ARE INCUBATED IN ZO HOUSE * WE ARE INCUBATED IN ZO HOUSE * WE ARE INCUBATED IN ZO HOUSE 
       </div>
       </div>
-      <div className=" relative md:hidden max-md:!block">
+      <div className=" relative md:hidden max-md:!block max-md: mt-16 max-md:w-[90%]">
      <img 
      className='absolute top-2 z-10'
       src="/Group 8.png"
      />
      <img 
-     className='top-5 z-0'
+     className=' z-0'
      src="/Group 9.png"
      />
 </div>
