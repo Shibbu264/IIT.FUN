1a85fb378314bbef30b8a94181307336570c1cea
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2025-02-26 19:29:03 +0530

    added userSlice

--- src/lib/store/slices/userSlice.ts
+++ src/lib/store/slices/userSlice.ts
@@ -1,13 +1,25 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface User {
+    name: string;
+    email: string;
+    image: string;
+    wallet: any; // Adjust type as necessary
+}
+
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        user: null,
+        user: null as User | null,
     },
     reducers: {
         setUser(state, action: PayloadAction<any>) {
             state.user = action.payload;
+        },
+        updateWallet(state,action){
+         if(state.user?.wallet){
+            state.user.wallet=action.payload
+         }
         },
         clearUser(state) {
             state.user = null;
