e9414bc0312ceeaa1d2be8a50e79867d6ca4ceb2
tirth2004 <tirth.bhayani.cse22@itbhu.ac.in>
2025-02-26 23:37:56 +0530

    Fixed IIT

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -30,23 +30,23 @@ export default function Landing() {
                 text-[1.5rem] max-sm:text-[0.95rem] sm:text-[1.4rem] md:text-[1.75rem] lg:text-[2.05rem]  xl:text-[2.55rem] 2xl:text-[2.55rem]">
                 <div className="grid grid-cols-3 gap-0 -my-[3px]">
                     <div className="whitespace-nowrap min-w-0 text-center">JEE TOPPED</div>
-                    <div className="whitespace-nowrap min-w-0 text-center">IIT FUN'D</div>
+                    <div className="whitespace-nowrap min-w-0 text-center tracking-[0.15em]">IIT FUN'D</div>
                     <div className="whitespace-nowrap max-sm:text-[0.85rem] max-md:text-[1.3rem] max-lg:text-[1.55rem] max-xl:text-[1.95rem] text-[2.40rem] min-w-0 text-center">DEGENGOD COMPLEXGEN</div>
                 </div>
                 <div className="grid grid-cols-3 gap-0 -my-[3px]">
                     <div className="whitespace-nowrap min-w-0 text-center">JEE TOPPED</div>
-                    <div className="whitespace-nowrap min-w-0 text-center">IIT FUN'D</div>
+                    <div className="whitespace-nowrap min-w-0 text-center tracking-[0.15em]">IIT FUN'D</div>
                     <div className="whitespace-nowrap max-sm:text-[0.85rem] max-md:text-[1.3rem] max-lg:text-[1.55rem] max-xl:text-[1.95rem] text-[2.40rem] min-w-0 text-center">DEGENGOD COMPLEXGEN</div>
                 </div>
                 <div className="grid grid-cols-3 gap-0 -my-[3px]">
                     <div className="whitespace-nowrap min-w-0 text-center">JEE TOPPED</div>
-                    <div className="whitespace-nowrap min-w-0 text-center">IIT FUN'D</div>
-                    <div className="whitespace-nowrap max-sm:text-[0.85rem] max-md:text-[1.3rem] max-lg:text-[1.55rem] max-xl:text-[1.95rem] text-[2.40rem] min-w-0 text-center">DEGENGOD COMPLEXGEN</div>
+                    <div className="whitespace-nowrap min-w-0 text-center tracking-[0.15em]">IIT FUN'D</div>
+                    <div className="whitespace-nowrap max-sm:text-[0.85rem] max-md:text-[1.3rem] max-lg:text-[1.95rem] max-xl:text-[2.45rem] text-[2.40rem] min-w-0 text-center">DEGENGOD COMPLEXGEN</div>
                 </div>
                 {Array.from({ length: 20 }, (_, i) => (
                     <div key={i} className="grid grid-cols-3 gap-0 -my-[3px]">
                         <div className="whitespace-nowrap min-w-0 text-center">JEE TOPPED</div>
-                        <div className="whitespace-nowrap min-w-0 text-center">IIT FUN'D</div>
+                        <div className="whitespace-nowrap min-w-0 text-center tracking-[0.15em]">IIT FUN'D</div>
                         <div className="whitespace-nowrap max-lg:ml-12 min-w-0 text-center">JEE TOPPED</div>
                     </div>
                 ))}
