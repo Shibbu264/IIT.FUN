b7d50785a98d7a20d04438463de7f7d00df8f4fe
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2025-03-18 01:23:21 +0530

    Fixed UI issues in leaderboard

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -6,16 +6,16 @@ import { TopRankers } from "@/components/Leaderboard/top-ranker"
 export default function Leaderboard() {
 
   return (
-    <div className="min-h-screen text-white">
-      <div className="container mx-auto px-4 py-8">
+    <div className="min-h-screen max-md:my-4 text-white">
+      <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold mb-10 text-center">Leaderboard</h1>
 
-        <Tabs defaultValue="institute" className="w-full max-w-xl mx-auto ">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="user" className="data-[state=active]:bg-[#c1ff00] data-[state=active]:text-black mx-auto">
+        <Tabs defaultValue="institute" className="w-full md:max-w-[80%] mx-auto ">
+          <TabsList className="flex gap-4 mx-auto w-full grid-cols-2">
+            <TabsTrigger value="user" className="data-[state=active]:bg-[#c1ff00] data-[state=active]:text-black">
               User
             </TabsTrigger>
-            <TabsTrigger value="institute" className="data-[state=active]:bg-[#c1ff00] data-[state=active]:text-black mx-auto">
+            <TabsTrigger value="institute" className="data-[state=active]:bg-[#c1ff00] data-[state=active]:text-black">
               Institute
             </TabsTrigger>
           </TabsList>
