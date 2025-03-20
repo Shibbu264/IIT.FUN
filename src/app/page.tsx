2de110f055776c37c377b074d04760d80f023c12
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2025-03-20 16:47:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import Landing from "@/components/Landing/Landing";
 import Dashboard from "@/components/Dashboard/Dashboard";
 import { authOptions } from "@/lib/utils";
+import { redirect } from "next/navigation";
 
 
 
@@ -9,13 +10,8 @@ import { authOptions } from "@/lib/utils";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  return (
-    <>
-      {!session ? (
+  return (!session ? (
         <Landing />
-      ) :
-        <Dashboard />
-      }
-    </>
+      ) : redirect("/contribute")
   );
 }
